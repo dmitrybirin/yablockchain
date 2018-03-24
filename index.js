1169d3a537c9042c004fc2518731116c0950dc45
Дмитрий Бирин <dmitrybirin@nx-1702.local>
2018-03-24 14:34:37 +0300

    transactions system created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const SHA256 = require('crypto-js/sha256')
+const uuidv4 = require('uuid/v4')
+
+class Transaction {
+    constructor(fromAddress, toAddress, amount, debug = true) {
+        this.fromAddress = fromAddress
+        this.toAddress = toAddress
+        this.amount = amount
+        if (debug) {
+            console.log(`${String(amount).padEnd(4)} :: ${String(fromAddress).padEnd(15)} -->     ${String(toAddress).padEnd(15)}`)
+        }
+    }
+}
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index
-        this.timestamp = timestamp
-        this.data = data
+    constructor(transactions, previousHash = '') {
+        this.id = uuidv4()
+        this.timestamp = +new Date()
+        this.transactions = transactions
         this.previousHash = previousHash
         this.hash = this.calculateHash()
         this.nonce = 0
@@ -34,28 +46,72 @@ class Block {
             BLOCK MINED!
             hash: ${this.hash}
             nonce: ${this.nonce}
-            time: ${(endTime-startTime)/1000}s`)
+            time: ${(endTime - startTime) / 1000}s`)
     }
 }
 
 class Blockchain {
     constructor(difficulty) {
         this.chain = [this.createGenesisBlock()]
+        this.pendingTransactions = []
+        this.pendingBlocks = []
+
         this.difficulty = difficulty
+        this.miningReward = 100
+        this.transactionsVolume = 2
     }
 
     createGenesisBlock() {
-        return new Block(0, +new Date(), 'Genesis block', '0')
+        return new Block([new Transaction(null, 'Alice', 1000)])
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1]
     }
 
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction)
+        if (this.pendingTransactions.length >= this.transactionsVolume) {
+            this.pendingBlocks.push(new Block(this.pendingTransactions))
+            this.pendingTransactions = []
+        }
+    }
+
+    minePendingBlock(miningRewardAddress) {
+        if (this.pendingBlocks.length) {
+            const block = this.pendingBlocks.shift()
+            block.mineBlock(this.difficulty)
+            this.chain.push(block)
+            this.pendingTransactions = [
+                new Transaction(null, miningRewardAddress, this.miningReward),
+            ]
+        } else {
+            console.warn('No pending blocks!')
+        }
+    }
+
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash
         newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount
+                }
+
+                if (trans.toAddress === address) {
+                    balance += trans.amount
+                }
+            }
+        }
+
+        return balance
     }
 
     isChainValid() {
@@ -77,11 +133,24 @@ class Blockchain {
 }
 
 let yablockchain = new Blockchain(4)
-console.log('Mining block 1...')
-yablockchain.addBlock(new Block(1, +new Date(), 'one string'))
 
-console.log('Mining block 2...')
-yablockchain.addBlock(new Block(2, +new Date(), 'another one string'))
+yablockchain.createTransaction(new Transaction('Alice', 'Bob', 100))
+yablockchain.createTransaction(new Transaction('Eva', 'Alice', 50))
+yablockchain.createTransaction(new Transaction('Minerguy', 'Alice', 150))
+yablockchain.createTransaction(new Transaction('Minerguy', 'Bob', 30))
 
+yablockchain.minePendingBlock('Minerguy')
+console.log(`
+    Minerguy mined block.
+    Bob's balance in system: ${yablockchain.getBalanceOfAddress('Bob')}
+    Minerguy's balance in system: ${yablockchain.getBalanceOfAddress('Minerguy')}
+`)
+
+yablockchain.minePendingBlock('Minerguy')
+console.log(`
+    Minerguy mined block.
+    Bob's balance in system: ${yablockchain.getBalanceOfAddress('Bob')}
+    Minerguy's balance in system: ${yablockchain.getBalanceOfAddress('Minerguy')}
+`)
 
 console.log(yablockchain.isChainValid())
